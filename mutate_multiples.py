57dc271078d9b7e26df72c8385c4c2022f577f16
Brian Connelly <bdc@bconnelly.net>
2016-06-20 11:10:23 -0700

    Updated mutate_multiples
    - Checked vertex attributes
    - Linted

--- mutate_multiples.py
+++ mutate_multiples.py
@@ -4,38 +4,45 @@ from numpy.random import exponential as nexp
 from scipy.special import gammainc as gamma
 from six.moves import range as srange
 
-def mutate_multiples(p, mutation_rate, genotype_counter):
+
+def mutate_multiples(population, mutation_rate, genotype_counter):
     """Mutate individuals in the population"""
 
-    assert(mutation_rate >= 0)
+    assert mutation_rate >= 0 and mutation_rate <= 1
 
-    num_mutants = nbinom(n = p.vs['abundance'], p = 1 - np.exp(-mutation_rate))
-    p.vs['abundance'] = p.vs['abundance'] - num_mutants
+    num_mutants = nbinom(n=population.vs['abundance'],
+                         p=1 - np.exp(-mutation_rate))
+    population.vs['abundance'] = population.vs['abundance'] - num_mutants
 
     r = num_mutants
     k = 1
     while any(r > 0):
-        #r is the number of individuals with at least k+1 mutations
-        #rate inside binomial is the prob of an individual having at least k+1 mutations given that it has at least k mutations
-        new_r = nbinom(n = r, p = gamma(k+1, mutation_rate)/gamma(k, mutation_rate), size = len(r))
-        num_k_mutants = np.subtract(r,new_r) #number of k-mutants
+        # r is the number of individuals with at least k+1 mutations
+        # rate inside binomial is the prob of an individual having at least
+        # k+1 mutations given that it has at least k mutations
+        new_r = nbinom(n=r,
+                       p=gamma(k+1, mutation_rate)/gamma(k, mutation_rate),
+                       size=len(r))
+        num_k_mutants = np.subtract(r, new_r) #number of k-mutants
         for parent_id in np.nonzero(num_k_mutants)[0]:
             for _mutant in srange(num_k_mutants[parent_id]):
-                mu_effect = nexp(scale = 0.01, size = k)
-                p.add_vertex(name = next(genotype_counter),
-                             abundance = 1,
-                             abundances = [1],
-                             total_abundance = 1,
-                             depth = p.vs[parent_id]['depth'] + 1,
-                             fitness = p.vs[parent_id]['fitness'] + np.prod(1 + mu_effect),
-                             fitness_effects = mu_effect.tolist(),
-                             fitness_diff = np.prod(1 + mu_effect),
-                             frequency = 0,
-                             max_frequency = 0,
-                             fixation_time = -1)
-                p.add_edge(source = parent_id, target = p.vcount() - 1,
-                           fitness_effect = np.prod(1 + mu_effect))
+                mu_effect = nexp(scale=0.01, size=k)
+                population.add_vertex(name=next(genotype_counter),
+                                      abundance=1,
+                                      abundances=[1],
+                                      total_abundance=1,
+                                      depth=population.vs[parent_id]['depth'] + 1,
+                                      fitness=population.vs[parent_id]['fitness'] + np.prod(1 + mu_effect),
+                                      fitness_effects=mu_effect.tolist(),
+                                      fitness_diff=np.prod(1 + mu_effect),
+                                      frequency=1 / population['population_size'],
+                                      max_frequency=0,
+                                      fixation_time=-1)
+                population.add_edge(source=parent_id,
+                                    target=population.vcount() - 1,
+                                    fitness_effect=np.prod(1 + mu_effect))
 
         r = new_r
         k += 1
-    return p
+    return population
+
