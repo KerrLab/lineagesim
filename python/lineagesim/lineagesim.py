51bd3cb2a4fa906fcb229b260b131e35a9a50f54
Brian Connelly <bdc@bconnelly.net>
2016-07-06 11:30:29 -0700

    Reorganizing/packaging first pass

---
+++ python/lineagesim/lineagesim.py
@@ -0,0 +1,216 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__version__ = "0.1.0"
+
+import csv
+import json
+
+import igraph
+import numpy as np
+from numpy import hstack as nhstack
+from numpy import nonzero as nnonzero
+from numpy import prod as nprod
+from numpy import subtract as nsubtract
+from numpy.random import binomial as nbinom
+from numpy.random import exponential as nexp
+from numpy.random import multinomial as nmultinom
+from numpy.random import normal as nnormal
+
+from scipy.special import gammainc as gamma
+
+from six.moves import range as srange
+
+
+# -----------------------------------------------------------------------------
+
+def create_population(population_size, counter, base_fitness=1.0):
+    """Create the population"""
+    pop = igraph.Graph(directed=True,
+                       graph_attrs={'population_size': population_size,
+                                    'generations': 0},
+                       vertex_attrs={'name': None,
+                                     'parent': None,
+                                     'first_seen': None,
+                                     'last_seen': None,
+                                     'lineage_last_seen': None,
+                                     'depth': None,
+                                     'abundance': 0,
+                                     'abundances': [0],
+                                     'lineage_abundance': 0,
+                                     'frequency': 0,
+                                     'max_frequency': 0,
+                                     'lineage_frequency': 0,
+                                     'fixation_time': -1,
+                                     'fitness': 0,
+                                     'fitness_effects': [0],
+                                     'fitness_diff': 0})
+
+    pop.add_vertex(name=next(counter),
+                   parent=-1,
+                   depth=0,
+                   abundance=population_size,
+                   abundances=[population_size],
+                   lineage_abundance=population_size,
+                   frequency=1.0,
+                   max_frequency=1.0,
+                   lineage_frequency=1.0,
+                   fixation_time=0,
+                   fitness=base_fitness,
+                   fitness_effects=[0],
+                   fitness_diff=0)
+    return pop
+
+
+def reproduce(population, population_size):
+    """Fill the population using fitness-proportional selection"""
+
+    fitnesses = np.array(population.vs['fitness'])
+    abundances = np.array(population.vs['abundance'])
+    ab_fit = fitnesses * abundances
+
+    population.vs['abundance'] = nmultinom(n=population_size,
+                                           pvals=ab_fit / ab_fit.sum(),
+                                           size=1)[0]
+    return population
+
+
+
+def mutate(population, mutation_rate, mutation_scale, genotype_counter):
+    """Mutate individuals in the population"""
+
+    assert mutation_rate >= 0 and mutation_rate <= 1
+
+    num_mutants = nbinom(n=population.vs['abundance'],
+                         p=1 - np.exp(-mutation_rate))
+    population.vs['abundance'] = population.vs['abundance'] - num_mutants
+
+    r = num_mutants
+    k = 1
+    while any(r > 0):
+        # r is the number of individuals with at least k+1 mutations
+        # rate inside binomial is the prob of an individual having at least
+        # k+1 mutations given that it has at least k mutations
+        new_r = nbinom(n=r,
+                       p=gamma(k+1, mutation_rate)/gamma(k, mutation_rate),
+                       size=len(r))
+        num_k_mutants = nsubtract(r, new_r) #number of k-mutants
+        for parent_id in nnonzero(num_k_mutants)[0]:
+            for _mutant in srange(num_k_mutants[parent_id]):
+                mu_effect = nexp(scale=mutation_scale, size=k)
+                mutant_fitness = nhstack((population.vs[parent_id]['fitness'], (1.0 + mu_effect))).prod()
+
+                population.add_vertex(name=next(genotype_counter),
+                                      parent=int(population.vs[parent_id]['name']),
+                                      abundance=1,
+                                      abundances=[1],
+                                      total_abundance=1,
+                                      depth=population.vs[parent_id]['depth'] + 1,
+                                      fitness=mutant_fitness,
+                                      fitness_effects=mu_effect.tolist(),
+                                      fitness_diff=mutant_fitness - population.vs[parent_id]['fitness'],
+                                      frequency=1.0 / population['population_size'],
+                                      max_frequency=1.0 / population['population_size'],
+                                      fixation_time=-1)
+                population.add_edge(source=parent_id,
+                                    target=population.vcount() - 1,
+                                    fitness_effect=nprod(1 + mu_effect))
+
+        r = new_r
+        k += 1
+    return population
+
+
+def prune_frequency(population, min_frequency):
+    """Delete extinct leaf nodes that did not reach a given frequency"""
+    # Could also prune based on lineage_abundance and max_frequency
+    population.vs.select(lambda v: v.outdegree() == 0 and v['abundance'] == 0 and v['first_seen'] is not None and v['max_frequency'] < min_frequency).delete()
+    return population
+
+
+def get_lineage_abundances(genotype):
+    """Get the abundance of a genotype and all of its descendants"""
+
+    genotype['lineage_abundance'] = genotype['abundance']
+
+    for child in genotype.neighbors(mode="OUT"):
+        genotype['lineage_abundance'] += get_lineage_abundances(genotype=child)
+
+    return genotype['lineage_abundance']
+
+# -----------------------------------------------------------------------------
+
+def graph_write_json(graph, filename, **kwargs):
+    """Write the graph to JSON file"""
+    d = {}
+    d['attributes'] = {a:graph[a] for a in graph.attributes()}
+#    d['vertices'] = [{'index': v.index,
+#                      'attributes': v.attributes()} for v in graph.vs]
+    d['vertices'] = [{'index': v.index,
+                      'attributes': {'name': v['name'],
+                                     'parent': v['parent'],
+                                     'first_seen': v['first_seen'],
+                                     'last_seen': v['last_seen'],
+                                     'lineage_last_seen': v['lineage_last_seen'],
+                                     'depth': v['depth'],
+                                     'abundance': int(v['abundance']),
+                                     'abundances': v['abundances'],
+                                     'lineage_abundance': int(v['lineage_abundance']),
+                                     'frequency': v['frequency'],
+                                     'max_frequency': v['max_frequency'],
+                                     'lineage_frequency': v['lineage_frequency'],
+                                     'fixation_time': v['fixation_time'],
+                                     'fitness': v['fitness'],
+                                     'fitness_effects': v['fitness_effects'],
+                                     'fitness_diff': v['fitness_diff']}} for v in graph.vs]
+
+    d['edges'] = [{'index': e.index,
+                   'source': e.source,
+                   'target': e.target,
+                   'attributes': e.attributes()} for e in graph.es]
+
+    with open(filename, 'w') as outfile:
+        json.dump(d, outfile, **kwargs)
+
+
+def graph_write_csv(graph, filename, **kwargs):
+    """Write the graph to CSV file"""
+
+    with open(filename, 'w') as outfile:
+        writer = csv.DictWriter(outfile, fieldnames=['Genotype',
+                                                     'Parent',
+                                                     'FirstSeen',
+                                                     'LastSeen',
+                                                     'LineageLastSeen',
+                                                     'Depth',
+                                                     'Fitness',
+                                                     'FitnessDiff',
+                                                     'NumMutations',
+                                                     'Abundance',
+                                                     'LineageAbundance',
+                                                     'Frequency',
+                                                     'MaxFrequency',
+                                                     'LineageFrequency',
+                                                     'FixationTime'])
+        writer.writeheader()
+
+        for g in graph.vs.select(lambda v: v['first_seen'] is not None):
+            writer.writerow({'Genotype': g['name'],
+                             'Parent': g['parent'],
+                             'FirstSeen': g['first_seen'],
+                             'LastSeen': g['last_seen'],
+                             'LineageLastSeen': g['lineage_last_seen'],
+                             'Depth': g['depth'],
+                             'Fitness': g['fitness'],
+                             'FitnessDiff': g['fitness_diff'],
+                             'NumMutations': len(g['fitness_effects']),
+                             'Abundance': g['abundance'],
+                             'LineageAbundance': g['lineage_abundance'],
+                             'Frequency': g['frequency'],
+                             'MaxFrequency': g['max_frequency'],
+                             'LineageFrequency': g['lineage_frequency'],
+                             'FixationTime': g['fixation_time']})
+
+
+# -----------------------------------------------------------------------------
+
---
+++ python/lineagesim/run_lineagesim.py
@@ -0,0 +1,200 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__version__ = "0.1.0"
+
+import argparse
+import datetime
+import csv
+import itertools
+import os
+import sys
+from warnings import warn
+
+import numpy as np
+
+from six.moves import range as srange
+
+from lineagesim import *
+
+
+# -----------------------------------------------------------------------------
+
+def parse_arguments():
+    """Parse command line arguments"""
+
+    def check_positive_01(value):
+        """Make sure a value is between 0 and 1, inclusive"""
+        fval = float(value)
+        if fval < 0 or fval > 1:
+            raise argparse.ArgumentTypeError("%s is not in the range [0,1]" % value)
+        return fval
+
+    def check_nonnegative(value):
+        """Make sure a value is non-negative"""
+        fval = float(value)
+        if fval < 0:
+            raise argparse.ArgumentTypeError("%s is not a valid non-negative value" % value)
+        return fval
+
+    def check_nonnegative_int(value):
+        """Make sure an integer value is non-negative"""
+        ival = int(value)
+        if ival < 0:
+            raise argparse.ArgumentTypeError("%s is not a valid non-negative integer value" % value)
+        return ival
+
+    def check_positive_int(value):
+        """Make sure an integer value is positive"""
+        ival = int(value)
+        if ival <= 0:
+            raise argparse.ArgumentTypeError("%s is not a valid positive integer value" % value)
+        return ival
+
+
+    parser = argparse.ArgumentParser(prog='lineagesim',
+                                     description='Run a lineage interference simulation')
+    parser.add_argument('--data_dir', '-d', metavar='DIR', default='data',
+                        help='Directory to store data (default: data)')
+    parser.add_argument('--fixation_freq', '-f', metavar='F',
+                        default=1.0, type=check_positive_01,
+                        help='Threshold frequency for classification as fixed (default: 1)')
+    parser.add_argument('--generations', '-G', metavar='G', default=1000,
+                        type=check_positive_int,
+                        help='Number of generations to simulate')
+    parser.add_argument('--mutation_rate', '-m', metavar='μ', default=1e-6,
+                        type=check_positive_01,
+                        help='Mutation rate (default: 1e-6)')
+    parser.add_argument('--mutation_scale', '-s', metavar='s', default=0.01,
+                        type=check_positive_01,
+                        help='Mutation scale (default: 0.01)')
+    parser.add_argument('--population_size', '-N', metavar='N',
+                        default=int(1e6), type=float,
+                        help='Size of the population (default: 1,000,000)')
+    parser.add_argument('--prune_freq', '-p', metavar='F',
+                        type=check_positive_01,
+                        help='Threshold frequency for pruning (default: (N * μ) / N)')
+    parser.add_argument('--seed', '-S', metavar='S', help='Set the '\
+                        'pseudorandom number generator seed',
+                        type=check_nonnegative_int)
+    parser.add_argument('--quiet', '-q', action='store_true', default=False,
+                        help='Suppress output messages')
+    parser.add_argument('--version', action='version', version=__version__)
+
+    return parser.parse_args()
+
+
+def run_simulation(args=parse_arguments()):
+    """Run the simulation"""
+
+    # Make sure population size is an integer. Doing the conversion here so
+    # that large numbers like 1e7, which are floats, can be given as args.
+    if int(args.population_size) != args.population_size:
+        msg = "Population size must be integer. Using {N}.".format(N=int(args.population_size))
+        warn(msg)
+    args.population_size = int(args.population_size)
+
+    if not args.prune_freq:
+        args.prune_freq = (args.population_size * args.mutation_rate) / args.population_size
+
+    if args.seed:
+        np.random.seed(args.seed)
+
+    if os.path.exists(args.data_dir):
+        newname = '{o}-{d}'.format(o=args.data_dir, d=datetime.datetime.now().strftime("%Y%m%d%H%M%S"))
+        msg = "{d} already exists. Saving data to {new}.".format(d=args.data_dir, new=newname)
+        warn(msg)
+        args.data_dir = newname
+
+    os.mkdir(args.data_dir)
+
+    outfile = csv.DictWriter(open(os.path.join(args.data_dir, "lineages.csv"),
+                                  'w'),
+                             fieldnames=['Generation',
+                                         'Genotype',
+                                         'Parent',
+                                         'FirstSeen',
+                                         'LastSeen',
+                                         'LineageLastSeen',
+                                         'Depth',
+                                         'Fitness',
+                                         'FitnessDiff',
+                                         'NumMutations',
+                                         'Abundance',
+                                         'LineageAbundance',
+                                         'Frequency',
+                                         'MaxFrequency',
+                                         'LineageFrequency',
+                                         'FixationTime'])
+    outfile.writeheader()
+
+    genotype_counter = itertools.count(0)
+
+    genotypes = create_population(population_size=int(args.population_size),
+                                  counter=genotype_counter)
+
+    for gen in srange(args.generations):
+        genotypes.vs.select(lambda v: v['first_seen'] is None)['first_seen'] = gen
+        genotypes.vs.select(lambda v: v['abundance'] > 0)['last_seen'] = gen
+        genotypes.vs.select(lambda v: v['lineage_abundance'] > 0)['lineage_last_seen'] = gen
+
+        if not args.quiet:
+            sys.stdout.write("\r")
+            pct = float(gen) / args.generations
+            sys.stdout.write("[%-40s] %d%%" % ('='* int(40 * pct), (pct * 100)))
+            sys.stdout.flush()
+
+        for g in genotypes.vs.select(lambda v: v['lineage_abundance'] > 0):
+            outfile.writerow({'Generation': gen,
+                              'Genotype': g['name'],
+                              'Parent': g['parent'],
+                              'FirstSeen': g['first_seen'],
+                              'LastSeen': g['last_seen'],
+                              'LineageLastSeen': g['lineage_last_seen'],
+                              'Depth': g['depth'],
+                              'Fitness': g['fitness'],
+                              'FitnessDiff': g['fitness_diff'],
+                              'NumMutations': len(g['fitness_effects']),
+                              'Abundance': g['abundance'],
+                              'LineageAbundance': g['lineage_abundance'],
+                              'Frequency': g['frequency'],
+                              'MaxFrequency': g['max_frequency'],
+                              'LineageFrequency': g['lineage_frequency'],
+                              'FixationTime': g['fixation_time']})
+
+        reproduce(population=genotypes, population_size=args.population_size)
+        mutate(population=genotypes,
+               mutation_rate=args.mutation_rate,
+               mutation_scale=args.mutation_scale,
+               genotype_counter=genotype_counter)
+
+        prune_frequency(population=genotypes, min_frequency=args.prune_freq)
+        get_lineage_abundances(genotype=genotypes.vs[0])
+
+        for v in genotypes.vs.select(lambda v: v['abundance'] > 0 and v['first_seen'] is not None):
+            v['abundances'].append(int(v['abundance']))
+
+        genotypes['generations'] += 1
+        genotypes['population_size'] = int(sum(genotypes.vs['abundance']))
+        genotypes.vs['frequency'] = genotypes.vs['abundance'] / sum(genotypes.vs['abundance'])
+        genotypes.vs['lineage_frequency'] = genotypes.vs['lineage_abundance'] / sum(genotypes.vs['abundance'])
+
+
+        for v in genotypes.vs.select(lambda v: v['abundance'] > 0 and v['first_seen'] is not None and v['frequency'] > v['max_frequency']):
+            v['max_frequency'] = v['frequency']
+
+        genotypes.vs.select(lambda v: v['fixation_time'] == -1 and (float(v['lineage_abundance']) / genotypes['population_size']) >= args.fixation_freq)['fixation_time'] = gen
+
+        # For writing the tree at every cycle
+        #genotypes.write_gml("TREES/genotypes-{0:06d}.gml".format(gen))
+
+    graph_write_csv(genotypes, os.path.join(args.data_dir, "lineages-end.csv"))
+    graph_write_json(genotypes, os.path.join(args.data_dir, "tree-end.json"),
+                     sort_keys=True)
+    genotypes.write_gml(os.path.join(args.data_dir, "tree-end.gml"))
+
+# -----------------------------------------------------------------------------
+
+if __name__ == "__main__":
+    run_simulation()
+
